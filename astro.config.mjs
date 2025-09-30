4c0f70661d60105d69e8bab59e0f296bb333db36
StephenYamasaki <theredstreak777@gmail.com>
2025-09-30 16:27:03 -0700

    Change output folder to dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
@@ -12,18 +12,23 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://stephenyamasaki.com/',
-    integrations: [mdx(), react(), tailwind({
-        applyBaseStyles: false,
-    }), icon(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            theme: 'plastic',
-            wrap: true,
-        },
+  site: "https://stephenyamasaki.com/",
+  integrations: [
+    mdx(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+    sitemap(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "plastic",
+      wrap: true,
     },
-    experimental: {
-        svg: true,
-    },
-    outDir: './docs'
+  },
+  experimental: {
+    svg: true,
+  },
 });
